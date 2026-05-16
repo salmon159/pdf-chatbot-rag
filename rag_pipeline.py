b41c861d6d47b03877a40e1cba1fef485406431f
salmon159 <salmon159@gmail.com>
2026-05-16 17:21:50 +0530

    Update rag_pipeline.py

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -1,11 +1,4 @@
-
 from transformers import pipeline
-
-from langchain_community.llms import HuggingFacePipeline
-
-from langchain.chains import RetrievalQA
-
-from langchain.prompts import PromptTemplate
 
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
@@ -15,61 +8,53 @@ from config import *
 
 from prompts import SYSTEM_PROMPT
 
-template = """
-{system_prompt}
-
-###Context:
-{context}
-
-###Question:
-{question}
-
-Answer:
-"""
-
-PROMPT = PromptTemplate(
-    template=template,
-    input_variables=["context", "question"],
-    partial_variables={
-        "system_prompt": SYSTEM_PROMPT
-    }
-)
-
+# Load embedding model
 embeddings = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2"
+    model_name=MODEL_NAME
 )
 
+# Load vector database
 vector_db = FAISS.load_local(
     VECTOR_DB_PATH,
     embeddings,
     allow_dangerous_deserialization=True
 )
 
+# Create retriever
 retriever = vector_db.as_retriever(
     search_kwargs={"k": 3}
 )
 
+# Load LLM
 pipe = pipeline(
     "text2text-generation",
-    model="google/flan-t5-base",
+    model=LLM_MODEL,
     max_new_tokens=256
-)
-
-llm = HuggingFacePipeline(
-    pipeline=pipe
-)
-
-qa_chain = RetrievalQA.from_chain_type(
-    llm=llm,
-    retriever=retriever,
-    chain_type="stuff",
-    chain_type_kwargs={
-        "prompt": PROMPT
-    }
 )
 
 def ask_question(query):
 
-    response = qa_chain.run(query)
+    # Retrieve relevant docs
+    docs = retriever.get_relevant_documents(query)
 
-    return response
+    context = "\n\n".join(
+        [doc.page_content for doc in docs]
+    )
+
+    prompt = f"""
+{SYSTEM_PROMPT}
+
+###Context:
+{context}
+
+###Question:
+{query}
+
+Answer:
+"""
+
+    result = pipe(prompt)
+
+    answer = result[0]["generated_text"]
+
+    return answer
