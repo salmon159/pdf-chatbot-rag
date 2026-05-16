93cac77e8fa1fc859a8e31c58b850665642fec2b
salmon159 <salmon159@gmail.com>
2026-05-16 19:25:28 +0530

    Update config.py 4

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
-
 MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2"
 
-LLM_MODEL = "mistralai/Mistral-7B-Instruct-v0.2"
+GGUF_REPO_ID = "TheBloke/Mistral-7B-Instruct-v0.2-GGUF"
 
-PDF_PATH = "data/manual.pdf"
+GGUF_FILENAME = "mistral-7b-instruct-v0.2.Q4_K_M.gguf"
 
 VECTOR_DB_PATH = "vectorstore/faiss_index"
+
+PDF_PATH = "data/manual.pdf"
 
 CHUNK_SIZE = 500
 
